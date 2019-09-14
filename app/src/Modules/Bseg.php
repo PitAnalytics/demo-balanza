6a4ae8ecd7bd8ddc02cbb47455da0575911efabe
HardCoreWare <bernardocodu@gmail.com>
2019-09-13 22:58:03 -0500

    mejorando consulta

--- app/src/Modules/Bseg.php
+++ app/src/Modules/Bseg.php
@@ -4,7 +4,7 @@ namespace App\Modules;
 //
 use App\Primitives\BigQueryConnection as Connection;
 //
-class Pac extends Connection{
+class Bseg extends Connection{
   
   private static $instance;
 
@@ -23,7 +23,8 @@ class Pac extends Connection{
   public function index(){
     
 
-    $index=$this->bigquery->query("SELECT * FROM `pit-analytics-2019.MULTIVA.bsegaio` LIMIT 1000");
+    $index=$this->bigquery->query(
+      "SELECT CONCAT(SUBSTR(BUDAT,0,4),'-',SUBSTR(BUDAT,5,2),'-',SUBSTR(BUDAT,7,2)) AS fecha, DMBTR AS monto, HKONT, SGTXT as texto AS cuenta FROM `pit-analytics-2019.MULTIVA.bsegaio`    ");
 
     return $index;
 
@@ -31,7 +32,7 @@ class Pac extends Connection{
 
   public function date(){
 
-    $date=$this->bigquery->query("");
+    $date=$this->bigquery->query("SELECT CONCAT(SUBSTR(BUDAT,0,4),'-',SUBSTR(BUDAT,5,2),'-',SUBSTR(BUDAT,7,2)) AS fecha");
 
     return $date;
 
