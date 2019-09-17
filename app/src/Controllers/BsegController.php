02e708976fd1e9bdbde195c6dd62be7d41fba907
HardCoreWare <bernardocodu@gmail.com>
2019-09-17 16:25:34 -0500

    listo

--- app/src/Controllers/BsegController.php
+++ app/src/Controllers/BsegController.php
@@ -106,6 +106,19 @@ class BsegController extends Controller{
 
     }
 
+    public function concepto($request,$response,$args){
+        
+        $superConcepto=$this->modules['bseg']->superConcepto();
+
+        $response1 = $response->withJson($superConcepto,201);
+        $response2 = $response1
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+
+        return $response2;
+
+    }
+
 }
 
 ?>
