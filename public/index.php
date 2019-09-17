75c6496ef224b0becbf4a09e5e8b4dbd8bc4462b
HardCoreWare <bernardocodu@gmail.com>
2019-09-17 15:38:22 -0500

    aumentando el buffer de salida

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 //
 error_reporting(E_ALL);
-ini_set("memory_limit","10000M");
+ini_set("memory_limit","14000M");
 //
 require_once '../app/vendor/autoload.php';
 require_once '../app/core/app.php';
